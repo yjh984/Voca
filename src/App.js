0cce451ddeb18d69c8c3219fa99f0ad21f1d2618
Jin <yjh984@daum.net>
2021-09-09 22:14:05 +0900

    v0.0 learning how to work the React!

--- src/App.js
+++ src/App.js
@@ -1,23 +1,39 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
+import Hello from './Components/Hello';
+import Welcome from './Components/Welcome';
+import styles from './App.module.css';
 
 function App() {
+  function showName(){
+    console.log('Tom~');
+  };
+  function showText(txt){
+    console.log(txt);
+  };
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Hello age={10}/>
+      <button onClick={showName}>Show Name</button>
+      <button onClick={()=>
+        console.log(30)
+        }>Show Age</button>
+      <div>
+        <input type='text' onChange={(e)=>{
+        const txt=e.target.value;
+        showText(txt);
+        }}/>
+      </div>
+      <Welcome/>
+      <Hello age={20}/>
+      <div className={styles.box}>
+        <div style={{backgroundColor: 'red'
+          , padding: '20px'
+          , margin: '20px'}}>A1</div>
+        <div style={{backgroundColor: 'yellow'
+          , padding: '20px'
+          , margin: '20px'}}>APP...</div>
+      </div>
     </div>
   );
 }
---
+++ src/Components/UserName.js
@@ -0,0 +1,3 @@
+export default function UseName({user}){
+    return <p>Hello! {user}</p>
+};
